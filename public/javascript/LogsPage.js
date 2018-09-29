5f43c47abc8d664c13ce87eb270f65cfc7e393aa
Timofeev Alexandr <taratatuy@yandex.ru>
2018-09-29 03:35:48 +0300

    Console web output fix

--- public/javascript/LogsPage.js
+++ public/javascript/LogsPage.js
@@ -1,17 +1,17 @@
 var consoleText = document.getElementById('consoleText');
 var consoleHead = document.getElementById('consoleHead');
 
-fetch('http://localhost:10202/').then(response => {
+fetch('http://79.139.225.60:10202/').then(response => {
   response.text().then(data => (consoleHead.innerHTML = data));
 });
 
-fetch('http://localhost:10202/consoleText', { method: 'POST' }).then(
+fetch('http://79.139.225.60:10202/consoleText', { method: 'POST' }).then(
   response => {
     response.text().then(data => (consoleText.innerHTML = data));
   }
 );
 
-var socket = io.connect('http://localhost:10202');
+var socket = io.connect('http://79.139.225.60:10202');
 socket.on('ConsoleText', data => {
   consoleText.innerHTML = data;
 });
